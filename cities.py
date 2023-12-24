71d23cddd85cd54a913f0d2d28d8abdb3f0c808c
steinmar004 <153025547+steinmar004@users.noreply.github.com>
2023-12-24 21:40:50 +0100

    Add files via upload

---
+++ cities.py
@@ -0,0 +1,16 @@
+
+cities = {
+'country': 'Indie',
+'population': 29399141,
+'fact': 'Najcieplejszym miesiącem jest czerwiec (śr. 33,4 °C), najchłodniejszym styczeń (śr. 14,2 °C). Najwięcej opadów (śr. 255 mm) przypada na sierpień'
+}
+Szanghaj = {
+'country': 'Chiny',
+'population':  26317104,
+'fact': 'Szanghaj jest uważany za miejsce narodzenia nowoczesnych Chin; był także przez pierwszą połowę XX wieku centrum kulturalnym Chin. '
+}
+
+cities = Szanghaj
+cities = Szanghaj
+for cont in cities():
+    print(cont)
